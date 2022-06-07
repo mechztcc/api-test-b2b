9729ca2b89a13e2420eacac5a4fedd5ebf6114bf
mechztcc <dojathan@gmail.com>
2022-06-07 10:10:35 -0300

    feat: add express and the first route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,14 @@
-console.log('Running');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server running at localhost:3333! ');
+});
