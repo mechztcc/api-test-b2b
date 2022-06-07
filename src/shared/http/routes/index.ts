be1f4938cf5a75f99bb43d17b7c2d1147227ba9d
mechztcc <dojathan@gmail.com>
2022-06-07 10:21:27 -0300

    feat: add default error handler

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,10 @@
+import AppError from '@shared/errors/AppError';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.get('/', (req, res) => {
-  return res.json('Sucesso!');
+  return res.json('Im Alive !');
 });
 
 export default routes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, response, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -8,6 +9,18 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res
+      .status(error.statusCode)
+      .json({ status: error.statusCode, message: error.message });
+  }
+
+  return res
+    .status(500)
+    .json({ status: 500, message: 'Internal server error!' });
+});
 
 app.listen(3333, () => {
   console.log('Server running at localhost:3333! ');
