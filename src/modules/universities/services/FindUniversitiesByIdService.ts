01d90d20c2535a4c02189c4895a9b6de9481c5fd
mechztcc <dojathan@gmail.com>
2022-06-08 09:46:47 -0300

    fix: validate when university has not found when try to find

--- src/modules/universities/services/FindUniversitiesByIdService.ts
+++ src/modules/universities/services/FindUniversitiesByIdService.ts
@@ -1,6 +1,7 @@
 import { getManager } from 'typeorm';
 import { ObjectID } from 'mongodb';
 import { University } from '../typeorm/entities/University';
+import AppError from '@shared/errors/AppError';
 
 interface IRequest {
   id: string;
@@ -10,9 +11,13 @@ export class FindUniversityByIdService {
   public async execute({ id }: IRequest): Promise<University> {
     const manager = getManager();
 
-    const university: University = await manager.findOneOrFail(University, {
+    const university: University = await manager.findOne(University, {
       _id: new ObjectID(id),
     });
+
+    if (!university) {
+      throw new AppError('Resource not found', 404);
+    }
 
     return university;
   }
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import 'express-async-errors';
 import express, { NextFunction, Request, response, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
